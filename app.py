b36371aae41be02614b6a86c1e85b01d679ed77a
jenny199 <uwizeyejeann@gmail.com>
2021-10-29 22:13:02 +0200

    added a text area for input text

--- app.py
+++ app.py
@@ -5,24 +5,48 @@ from model import TextSummarize
 import math
 
 app = Flask(__name__)
-
+def preprocessing(text):
+  article_text = re.sub(r'\[[0-9]*\]', ' ', text)
+  article_text = re.sub(r'\s+', ' ', article_text)
+  article_text=article_text.replace("\"", "")
+  return article_text
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
-@app.route('/', methods=['POST'])
+
+@app.route('/summarize', methods=['POST'])
 def predict():
 
-    #text = request.form.get('text')
+    text = request.form.get('text')
+    # textfile = request.files['textfile']
+    # text_path = "./text" + textfile.filename
+    # textfile.save(text_path)
+    # text = open(text_path,"r")
+    # article_text = text.read()
+    summary = preprocessDataAndSummarize(text)
+    
+
+    return render_template('index.html', input = text, prediction = summary)
+
+@app.route('/summarizeUploaded', methods=['POST'])
+def predictupload():
+
+    # text = request.form.get('text')
     textfile = request.files['textfile']
     text_path = "./text" + textfile.filename
     textfile.save(text_path)
     text = open(text_path,"r")
     article_text = text.read()
-    summary = preprocessDataAndSummarize(article_text)
+
+    #calling a preprocessing function
+    article_text = preprocessing(article_text)
+    
+    #calling a text summarizer model
+    textsummary = TextSummarize(article_text, threshold=0.01,ratio=0.5)
+    summary = textsummary.summary
     
 
     return render_template('index.html', input = article_text, prediction = summary)
-
 
 def preprocessDataAndSummarize(text):
 
